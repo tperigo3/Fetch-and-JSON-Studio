e6dce2c503de539d2b284e9c4c510a59413595ef
Garner Perigo <tperigo3@gmail.com>
2020-10-26 18:16:53 -0500

    finish studio

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 // TODO: add code here
+window.addEventListener('load', function(){
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+        .then(function(response)
+            {return response.json()}
+        ).then(function(json){
+            const container = document.getElementById('container');
+            let astronauts = '';
+            for(astronaut of json){
+                astronauts += `
+                <div class="astronaut">
+                    <div class="bio">
+                        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                        <ul>
+                            <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                            <li>Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills}</li>
+                        </ul>
+                    </div>
+                    <img class ="avatar" src=${astronaut.picture}>
+                </div>
+                `
+            }
+            container.innerHTML = astronauts;
+        })
+            
+});
+function checkIfActive(astronaut){
+    if (astronaut.active === true){
+        return `<span style="color:green">${astronaut.active}</span>`
+    }else{
+        return astronaut.active;
+    }
+
+}
